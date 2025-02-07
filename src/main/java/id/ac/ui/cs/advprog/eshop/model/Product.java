c2a7ea2c2faae8e07507457a622d1e5171adbf27
Syauqi Yasman <146733124+syauqiyasman@users.noreply.github.com>
2025-02-07 14:03:14 +0700

    Create Product.java

---
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Product.java
@@ -0,0 +1,16 @@
+package id.ac.ui.cs.advprog.eshop.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class Product {
+
+    private String productId;
+
+    private String productName;
+
+    private int productQuantity;
+
+}
