acbaeace849328a9048f9d5526e7cbd3e5c0f4f4
Syauqi Yasman <146733124+syauqiyasman@users.noreply.github.com>
2025-02-09 11:58:49 +0700

    renaming function

--- src/test/java/id/ac/ui/cs/advprog/eshop/functional/HomePageFunctionalTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/functional/HomePageFunctionalTest.java
@@ -38,7 +38,7 @@ public class HomePageFunctionalTest {
     }
 
     @Test
-    void welcomeMessage_homepage_isCorrect(ChromeDriver driver) throws Exception {
+    void welcomeMessage_homePage_isCorrect(ChromeDriver driver) throws Exception {
         driver.get(baseUrl);
         String welcomeMessage = driver.findElement(By.tagName("h3")).getText();
         assertEquals("Welcome", welcomeMessage);
