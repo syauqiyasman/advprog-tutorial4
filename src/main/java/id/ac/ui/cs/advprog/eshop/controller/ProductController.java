b9e622495920e20f93f73367c1d9749080f266f5
Syauqi Yasman <146733124+syauqiyasman@users.noreply.github.com>
2025-02-16 15:39:22 +0700

    fix template error

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
@@ -28,7 +28,7 @@ public class ProductController {
     public String createProductPage(Model model) {
         Product product = new Product();
         model.addAttribute("product", product);
-        return "createProduct";
+        return "CreateProduct";
     }
 
     @PostMapping("/create")
@@ -41,7 +41,7 @@ public class ProductController {
     public String editProductPage(@PathVariable("id") String productId, Model model) {
         Product product = service.findProductById(productId);
         model.addAttribute("product", product);
-        return "editProduct";
+        return "EditProduct";
     }
 
     @PostMapping("/edit/{id}")
@@ -61,7 +61,7 @@ public class ProductController {
     public String productListPage(Model model) {
         List<Product> allProducts = service.findAll();
         model.addAttribute("products", allProducts);
-        return "productList";
+        return "ProductList";
     }
 
 }
