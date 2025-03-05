c0d7566e4a9ee5f691903b4d5702438f28e51d1b
Syauqi Yasman <146733124+syauqiyasman@users.noreply.github.com>
2025-03-05 23:49:34 +0700

    [REFACTOR] Apply PaymentMethod and PaymentStatus to Payment model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/Payment.java
@@ -1,5 +1,7 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
+import id.ac.ui.cs.advprog.eshop.enums.PaymentMethod;
+import id.ac.ui.cs.advprog.eshop.enums.PaymentStatus;
 import lombok.Getter;
 
 import java.util.Map;
