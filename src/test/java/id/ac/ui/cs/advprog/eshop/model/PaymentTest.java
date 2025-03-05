c0603e40af6bab17ea99ce5dffcdd6aa296ea698
Syauqi Yasman <146733124+syauqiyasman@users.noreply.github.com>
2025-03-05 23:50:33 +0700

    [REFACTOR] Apply PaymentMethod and PaymentStatus to PaymentTest

--- src/test/java/id/ac/ui/cs/advprog/eshop/model/PaymentTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/model/PaymentTest.java
@@ -1,6 +1,8 @@
 package id.ac.ui.cs.advprog.eshop.model;
 
 import id.ac.ui.cs.advprog.eshop.enums.OrderStatus;
+import id.ac.ui.cs.advprog.eshop.enums.PaymentMethod;
+import id.ac.ui.cs.advprog.eshop.enums.PaymentStatus;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
